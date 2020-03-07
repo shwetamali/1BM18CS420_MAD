449fb3b02c1ebe6f602771e19992679964e3a12f
shwetamali <53892753+shwetamali@users.noreply.github.com>
2020-03-07 12:33:55 +0530

    Update second_activity.java

--- Student/second_activity.java
+++ Student/second_activity.java
@@ -1,30 +1,33 @@
-package com.example.student;
-
+package com.example.second;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+public class secondActivity extends AppCompatActivity {
 
-public class sec_activity extends AppCompatActivity {
-    TextView t1,t2,t3;
-    String name,reg,dept;
+        TextView t1,t2,t3;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sec_activity);
-        t1=(TextView)findViewById(R.id.textView5);
-        t2=(TextView)findViewById(R.id.textView6);
-        t3=(TextView)findViewById(R.id.textView7);
+        String name,reg,dept;
 
-        Intent i=getIntent();
-        name=i.getStringExtra("name_key");
-        reg=i.getStringExtra("reg_key");
-        dept=i.getStringExtra("dept");
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_second);
 
-         t1.setText(name);
-        t2.setText(reg);
-        t3.setText(dept);
-    }
+                t1= (TextView) findViewById(R.id.textView1);
+                t2= (TextView) findViewById(R.id.textView2);
+                t3= (TextView) findViewById(R.id.textView3);
+
+
+                Intent i = getIntent();
+
+                name=i.getStringExtra("name_key");
+                reg=i.getStringExtra("reg_key");
+                dept=i.getStringExtra("dept_key");
+
+                t1.setText(name);
+                t2.setText(reg);
+                t3.setText(dept);
+
+        }
 }
