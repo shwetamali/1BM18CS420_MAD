e5f34c6cc54b8d22d8da6defd20d8aefb031cdcf
shwetamali <53892753+shwetamali@users.noreply.github.com>
2020-03-07 12:33:23 +0530

    Update First_activity.java

--- Student/First_activity.java
+++ Student/First_activity.java
@@ -1,62 +1,59 @@
-package com.example.student;
-
+package com.example.second;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner s;
-    Button b1;
-    EditText et1,et2;
-    TextView tv,tv1,tv2,tv3;
 
-    String[] dept_array={"CSE","ECE","ISE","MECH","CVIL"};
-    String name,reg,dept;
+        //Defining the Views
+        EditText e1,e2;
+        Button bt;
+        Spinner s;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tv=(TextView) findViewById(R.id.textView);
-        b1=(Button) findViewById(R.id.button);
-        tv1=(TextView) findViewById(R.id.textView2);
-        et1=(EditText) findViewById(R.id.editText);
-        tv2=(TextView) findViewById(R.id.textView3);
-        et2=(EditText) findViewById(R.id.editText2)
-        tv3=(TextView) findViewById(R.id.textView4);
-        s=(Spinner) findViewById(R.id.spinner);
+        //Data for populating in Spinner
+        String [] dept_array={"CSE","ECE","IT","Mech","Civil"};
 
-        ArrayAdapter  adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item);
-        s.setAdapter(adapter);
+        String name,reg,dept;
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                name=et1.getText().toString();
-                reg=et2.getText().toString();
-                dept=s.getSelectedItem().toString();
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
 
+                //Referring the Views
+                e1= (EditText) findViewById(R.id.editText);
+                e2= (EditText) findViewById(R.id.editText2);
 
-                Intent i = new Intent(MainActivity.this,sec_activity.class);
+                bt= (Button) findViewById(R.id.button);
 
-                i.putExtra("name_key",name);
-                i.putExtra("reg_key",reg);
-                i.putExtra("dept_key",dept);
+                s= (Spinner) findViewById(R.id.spinner);
 
-                startActivity(i);
+                ArrayAdapter adapter= new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item,dept_array);
+                s.setAdapter(adapter);
+
+                bt.setOnClickListener(new View.OnClickListener() {
+
+                        @Override public void onClick(View v) {
+
+                                name=e1.getText().toString();
+                                reg=e2.getText().toString();
+                                dept=s.getSelectedItem().toString();
+
+                                Intent i = new Intent(MainActivity.this, secondActivity.class);
 
 
-            }
-        });
+                                i.putExtra("name_key", name);
+                                i.putExtra("reg_key",reg);
+                                i.putExtra("dept_key", dept);
 
+                                startActivity(i);
 
-
-    }
+                        }
+                });
+        }
 }
